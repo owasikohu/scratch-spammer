f2772a4686d133a1ce39754ac9169e3254d46414
owasikohu <owatas2022@gmail.com>
2024-02-21 22:43:41 +0900

    Update spammer.js

--- spammer.js
+++ spammer.js
@@ -1,1 +1,1 @@
-spammer
+let messege=prompt("メッセージを入力");alert(messege);let studio=prompt("スタジオのidを入力");alert(studio);let url="https://api.scratch.mit.edu/proxy/comments/studio/"+studio,countlimit=prompt("送信回数を入力"),body=`{"content":"${messege}","parent_id":"","commentee_id":""}`,x=0;for(;x<countlimit;)x+=1,fetch(url,{headers:{"content-type":"application/json","x-csrftoken":scratchAddons.globalState.auth.csrfToken,"x-token":scratchAddons.globalState.auth.xToken},body:body,method:"POST",credentials:"include"});
